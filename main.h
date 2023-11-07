4e5933df0b9143df2a72804bd23f2f1a9f659532
Sasuke-kh <omerkhalid56682@gmail.com>
2023-11-07 16:23:45 +0200

    added unsuccesfull header and strtoc

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#define BUFSIZE 1204
+
+#endif /* MAIN_H */
