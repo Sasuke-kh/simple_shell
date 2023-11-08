ae19a644daf3d338bff6d1707b09ab89d5feaa3a
bashar-karoury <basharalkaroury@hotmail.com>
2023-11-08 20:21:01 +0200

    Get command and tokenize it

--- main.c
+++ main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 
 int sh_script(char **argv, char **env);
 int sh_non_interactive(char **env);
 int sh_interactive(char **env);
+int get_command(char **av, int *ac);
+int free_av_memory(char **av, int ac);
 
 int main(int argc, char **argv, char **env)
 {
@@ -39,24 +42,33 @@ int sh_interactive(char **env)
 	size_t n;
 	int status = 1;
 	int ac;
-	char **av;
-
-	while (status)
+	char **av = NULL;
+	int i = 0;
+	av = (char **)malloc(10 * sizeof(char *));
+	while (status--)
 	{
 		printf("($) ");
-		status = get_command(av, ac);
+		get_command(av, &ac);
 		if(status == -2)
 			continue;
+		for (i = 0; i < ac; i++)
+			printf("command[%d] = %s\n", i, av[i]);
+			printf("ac = %d\n", ac);
+		 free_av_memory(av, ac);
 	}
+	free(av);
 }
 
-int get_command(char **av, int ac)
+int get_command(char **av, int *ac)
 {
 	char *line;
+	char *copy_line;
 	ssize_t size;
 	size_t n = 0;
-
+	char* token;
+	int i = 0;
 	line = (char *)malloc(100);
+	copy_line = line;
 	size = getline(&line, &n, stdin);
 	if (size == -1)
 		return (-2);
@@ -66,4 +78,27 @@ int get_command(char **av, int ac)
 		line[size - 1] = '\0';
 	printf("command  = %s\n", line);
 
+	token = strtok(line, " ");
+	while (token != NULL)
+	{
+		printf(" %s\n", token);
+		av[i] = strdup(token);
+		token = strtok(NULL, " ");
+		i++;
+	}
+	*ac = i;
+	free(line);
+	free(token);
+	free(copy_line);
+}
+
+int free_av_memory(char **av, int ac)
+{
+	int i = 0;
+
+	for (i = 0; i < ac; i++)
+	{
+		printf("freeing [%d] = %s\n", i, av[i]);
+		free(av[i]);
+	}
 }
