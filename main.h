1ded7338de7915bc822229ab971dda3fedce8f35
Sasuke-kh <omerkhalid56682@gmail.com>
2023-11-09 12:57:09 +0200

    getline function with a simple testcase

--- main.h
+++ main.h
@@ -10,7 +10,9 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #define BUFSIZE 1204
+#define INITIAL_BUFFER_SIZE 128
 
+ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
 char *_strtok(char *str, const char *delim);
 
 #endif /* MAIN_H */
---
+++ tests/_getline-test.c
@@ -0,0 +1,18 @@
+#include "../main.h"
+
+
+int main()
+{
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+
+	printf("$ ");
+	read = _getline(&line, &len, stdin);
+	if (read != -1)
+		printf("You entered: %s\n", line);
+	else
+		printf("Error reading the line.\n");
+	free(line);
+	return (0);
+}
