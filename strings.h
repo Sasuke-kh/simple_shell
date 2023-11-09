3543879e45da5ab8bcca33a9b8fab9b43236c259
Sasuke-kh <omerkhalid56682@gmail.com>
2023-11-09 21:37:05 +0200

    added strings functions

---
+++ strings.h
@@ -0,0 +1,12 @@
+#ifndef STRINGS_H
+#define STRINGS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+
+#endif /* STRINGS_H */
