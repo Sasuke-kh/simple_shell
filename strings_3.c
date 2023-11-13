6d32a14573dbf7f998e1cdc3b91296479dedbe15
Sasuke-kh <omerkhalid56682@gmail.com>
2023-11-13 21:10:59 +0200

    strings update

---
+++ strings_3.c
@@ -0,0 +1,46 @@
+#include "strings.h"
+#include <unistd.h>
+
+/**
+ * print_str - print string
+ * @str: givan string
+ * Return: void
+ */
+
+void print_str(char *str)
+{
+	int count;
+
+	for (count = 0; str[count] != '\0'; count ++)
+	{
+		_putchar(str[count]);
+	}
+}
+
+/**
+ * main - entry point
+ *
+ * Description: print _putchar
+ *
+ * Return: 0 (success)
+ */
+int _puterror(char c)
+{
+	return (write(1, &c, 2));
+}
+
+/**
+ * print_error - print string
+ * @str: givan string
+ * Return: void
+ */
+
+void print_error(char *str)
+{
+	int count;
+
+	for (count = 0; str[count] != '\0'; count ++)
+	{
+		_puterror(str[count]);
+	}
+}
