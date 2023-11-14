b458011fd8c3e284f6c529d2f8f8c573e34f9115
bashar-karoury <basharalkaroury@hotmail.com>
2023-11-14 16:21:12 +0200

    Refactor main to use one handler instead of 3

--- main.c
+++ main.c
@@ -12,16 +12,13 @@
 #include "memory_manager.h"
 #include <signal.h>
 #include "env.h"
+#include <fcntl.h>
 
 extern char **environ;
 
 typedef void (*sighandler_t)(int);
 
-int sh_script(char **argv, char **env);
-int sh_non_interactive(char **env);
-int sh_interactive(char **env);
-void free_av_memory(char **av, int ac);
-	
+int sh_handler(char **env, int fd, int is_ineractive);
 void exit_handler(int i)
 {
 	free_manager(NULL);
@@ -29,142 +26,47 @@ void exit_handler(int i)
 }
 int main(int argc, char **argv, char **env)
 {
-	sighandler_t sig = signal(SIGINT, exit_handler);
+	sighandler_t sig;
+	int is_interactive = 0;
+	int fd;
+	int exit_status;
+	sig = signal(SIGINT, exit_handler);
 	if (sig == SIG_ERR)
+	{
 		printf("ERROR\n");
+		return(-5);
+	}
 	if (heap_environ())
+	{
 		printf("ERROR Can't heap environ\n");
+		return (-7);
+	}
+	
 	if (argc > 1)
 	{
-		sh_script(argv, environ);
+		fd  = open(argv[1], O_RDONLY);
+		if (fd < 0)
+		{
+			printf("Can't open %s\n", argv[1]);
+			return (-4);
+		}
 	}
 	else if (argc == 1)
 	{
 		if (isatty(0))
 		{
-			sh_interactive(environ);
+			is_interactive = 1;
 		}
 		else
 		{
-			sh_non_interactive(environ);
 		}
+		fd = STDIN_FILENO;
 	}
-
-	return (0);
+	exit_status = sh_handler(environ, fd, is_interactive);
+	return (exit_status);
 }
 
-int sh_script(char **argv, char **env)
-{
-	int ac;
-	char **av = NULL;
-	list_t *paths_head = NULL;
-	int done = 0;
-	int fd;
-
-	fd  = open(argv[1], O_RDONLY);
-	if (fd < 0)
-	{
-		printf("Can't open %s\n", argv[1]);
-		return (-4);
-	}
-
-	av = (char **)_malloc(100 * sizeof(char *));  /*Is 100 enough ? */
-	if (av == NULL)
-		return (-1);
-
-	while (!(get_command_from_file(fd, av, &ac)))
-	{
-		done = 0;
-		if (get_PATH(env, &paths_head))
-		{
-			printf("Error!! Can't get path\n");
-		}
-		else
-		{	
-			if (is_found_and_excecutable(av, paths_head))
-			{
-				//printf("XXXXXXXXError!! Command not found\n");
-			}
-			else
-			{
-				done = 1;
-				if (fork_and_execve(av, env))
-				{
-					//printf("Error!! Commmand can't get executed\n");
-				}
-				else
-				{
-				}
-			}
-			if (!done)
-			{
-				//printf("check command\n");
-				if (is_built_in_commnad(av, &ac))
-				{
-					printf("hsh : not found\n");
-				}
-			}
-		}
-		free_av_memory(av, ac);
-	}
-    free_and_NULL(&av);
-	free_list(paths_head);
-	free_manager(NULL);
-	return (0);
-}
-
-int sh_non_interactive(char **env)
-{
-	int ac;
-	char **av = NULL;
-	list_t *paths_head = NULL;
-	int done = 0;
-
-	av = (char **)_malloc(100 * sizeof(char *));  /*Is 100 enough?*/
-	if (av == NULL)
-		return (-1);
-
-	while (!(get_command_from_file(0, av, &ac)))
-	{	
-		done = 0;
-		if (get_PATH(env, &paths_head))
-		{
-			printf("Error!! Can't get path\n");
-		}
-		else
-		{
-			if (is_found_and_excecutable(av, paths_head))
-			{
-
-			}
-			else
-			{
-				if (fork_and_execve(av, env))
-				{
-					//printf("Error!! Commmand can't get executed\n");
-				}
-				else
-				{
-				}
-				done = 1;
-			}
-			if (!done)
-			{
-				if (is_built_in_commnad(av, &ac))
-				{
-					printf("hsh : not found\n");
-				}
-			}
-		}
-		free_av_memory(av, ac);
-
-	}
-	free_and_NULL(&av);
-    free_list(paths_head);
-    free_manager(NULL);
-	return (0);
-}
-int sh_interactive(char **env)
+int sh_handler(char **env, int fd, int is_interactive)
 {
 	int status = 1;
 	int ac;
@@ -177,18 +79,23 @@ int sh_interactive(char **env)
 		return (-1);
 	while (status)
 	{
-		done = 0;	
-		printf("($) ");
-		fflush(stdout);
-		c_s = get_command(av, &ac);
+		done = 0;
+		if(is_interactive)
+		{	
+			printf("($) ");
+			fflush(stdout);
+		}
+		c_s = get_command_from_file(fd, av, &ac);
 		if (c_s == -1)
-		{
+		{	
+			/*Should we continue or exit ??????*/
 			//printf("Error!! Can't get command\n");
 			continue;
 		}
 		else if (c_s == -2)			/* EOF */
 		{
-			printf("\n");
+			if(is_interactive)
+				printf("\n");
 			break;
 		}	
 		if (get_PATH(env, &paths_head))
@@ -205,7 +112,7 @@ int sh_interactive(char **env)
 			{
 				if (fork_and_execve(av, env))
 				{
-				//	printf("Error!! Commmand can't get executed\n");
+					//	printf("Error!! Commmand can't get executed\n");
 				}
 				else
 				{
@@ -225,7 +132,8 @@ int sh_interactive(char **env)
 
 	}
 	free_and_NULL(&av);
-    free_list(paths_head);
-    free_manager(NULL);
+	free_list(paths_head);
+	free_manager(NULL);
 	return (0);
 }
+
