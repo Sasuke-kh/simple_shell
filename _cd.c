6799c0326247c3b3dcccd6f620cfb4cf4661b9f4
Sasuke-kh <omerkhalid56682@gmail.com>
2023-11-14 22:11:19 +0200

    cd fix

--- _cd.c
+++ _cd.c
@@ -9,6 +9,7 @@ int change_ev(const char *current, char *current_directory, const char *prev);
 /**
  * _cd - change current working directory
  * @av: arguments
+ * @ac: arguments count
  * Return: 0 on success -1 on failure
  */
 
@@ -17,12 +18,17 @@ int _cd(char **av, int *ac)
 	const char *home = _getenv("HOME");
 	const char *prev = _getenv("OLDPWD");
 	const char *current = _getenv("PWD");
-	char current_directory[4096];
+	char current_directory[1024];
 
 	if (*ac == 1)
 	{
 		if (cd_home(home) == 0)
-			return (0);
+		{
+			if (change_ev(current, current_directory, prev) == 0)
+				return (0);
+			else
+				return (-1);
+		}
 		else
 			return (-1);
 	}
@@ -60,10 +66,7 @@ int _cd(char **av, int *ac)
 				return (-1);
 		}
 		else
-		{
-			perror(av[1]);
 			return (1);
-		}
 	}
 	return (-1);
 }
@@ -79,14 +82,9 @@ int cd_home(const char *home)
 	if (home != NULL)
 	{
 		if (chdir(home) == 0)
-		{
 			return (0);
-		}
 		else
-		{
-			perror(home);
 			return (-1);
-		}
 	}
 	else
 		return (-1);
@@ -104,10 +102,13 @@ int change_ev(const char *current, char *current_directory, const char *prev)
 {
 	if (_setenv(prev, current, 1) == 0)
 	{
-		if (getcwd(current_directory, sizeof(current_directory)) != NULL)
+		if (getcwd(current_directory, 1024) != NULL)
 		{
 			if (_setenv(current, current_directory, 1) == 0)
+			{
+				printf("\n\n%s\n\n", prev);
 				return (0);
+			}
 			else
 				return (-1);
 		}
