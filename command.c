a0413e6e1ed1557ed019451d8e042bc23ebeaf8d
Sasuke-kh <omerkhalid56682@gmail.com>
2023-11-16 00:49:08 +0200

    betty

--- command.c
+++ command.c
@@ -1,22 +1,29 @@
 #include "command.h"
 #include "strings.h"
 
+/**
+ * is_built_ib_command - like the name ;)
+ * @av: args
+ * @ac: args count
+ * @exit_status: like the name
+ * Return: idk
+ */
+
 int is_built_in_commnad(char **av, int *ac, int *exit_status)
 {
-    command_t commands[NO_COMMANDS] = {{"cd" , _cd}, {"exit", shell_exit}, {"env", shell_env},{"setenv", shell_setenv },{"unsetenv", shell_unsetenv}};
-    int i = 0;
-    int result = -1;
+	command_t commands[NO_COMMANDS] = {{"cd", _cd}, {"exit", shell_exit}, {"env", shell_env}, {"setenv", shell_setenv }, {"unsetenv", shell_unsetenv}};
+	int i = 0;
+	int result = -1;
 
-    while (i < NO_COMMANDS)
-    {
-        result =  _strcmp(av[0], commands[i].command);
-        if (result == 0)
-        {
-            commands[i].function(av, ac, exit_status);
-            return (0);
-        }
-        i++;
-    }
-    return (-1);
+	while (i < NO_COMMANDS)
+	{
+		result =  _strcmp(av[0], commands[i].command);
+		if (result == 0)
+		{
+			commands[i].function(av, ac, exit_status);
+			return (0);
+		}
+		i++;
+	}
+	return (-1);
 }
-
