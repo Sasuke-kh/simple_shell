322af0382785139d15cd3691744b1194c837f5b5
Sasuke-kh <omerkhalid56682@gmail.com>
2023-11-16 00:49:55 +0200

    betty

--- helpers.c
+++ helpers.c
@@ -9,12 +9,20 @@
 #include <string.h>
 #include "memory_manager.h"
 
+/**
+ * get_command_from_file - gets commands from a file
+ * @fd: idk
+ * @av: args
+ * @ac: args count
+ * Return: idk
+ */
+
 int get_command_from_file(int fd, char **av, int *ac)
 {
 	char *line;
 	ssize_t size;
 	size_t n = 10000;
-	char* token;
+	char *token;
 	int i = 0;
 
 	line = (char *)_malloc(10000);
@@ -47,9 +55,18 @@ int get_command_from_file(int fd, char **av, int *ac)
 	}
 	if (i == 0)
 		return (-3);
-	av[i] = NULL;*ac = i;
+	av[i] = NULL;
+	*ac = i;
 	return (0);
 }
+
+/**
+ * fork_and_execve - excecution i gess
+ * @av: args
+ * @env: idk
+ * @exit_status: like the name ;)
+ * Return: idk
+ */
 
 int fork_and_execve(char **av, char **env, int *exit_status)
 {
@@ -58,21 +75,21 @@ int fork_and_execve(char **av, char **env, int *exit_status)
 
 	pid = fork();
 	if (pid == -1)
-		return -1;
+		return (-1);
 
-	if(pid == 0)
+	if (pid == 0)
 	{
-		if(execve(av[0], av, env) == -1)
+		if (execve(av[0], av, env) == -1)
 		{
 			perror("Errorx!!\n");
-			return -2;
+			return (-2);
 		}
 	}
 	else
 	{
 		if (waitpid(pid, &status, 0) != pid)
 		{
-			return -3;
+			return (-3);
 		}
 		else
 		{
@@ -80,16 +97,23 @@ int fork_and_execve(char **av, char **env, int *exit_status)
 			if (WIFEXITED(status) != 1)
 			{
 				perror("Error \n");
-				return -4;
+				return (-4);
 			}
 		}
 
 	}
-	return 0;
+	return (0);
 }
 
+/**
+ * get_PATH - excecution i gess
+ * @env: args
+ * @paths_head: idk
+ * Return: idk
+ */
+
 int get_PATH(char **env, list_t **paths_head)
-{	
+{
 	int i = 0;
 	int j = 0;
 	int result = -1;
@@ -99,14 +123,14 @@ int get_PATH(char **env, list_t **paths_head)
 
 	/*	if(*paths_head == NULL)   */
 	{
-		while(env[i] != NULL && result != 0)
+		while (env[i] != NULL && result != 0)
 		{
 			result =  _strncmp("PATH=", env[i], 5);
 			i++;
 		}
 		if (result == -1)
 			return (-1);
-		pathLine = env[i-1];
+		pathLine = env[i - 1];
 		copy_pathLine = _strdup(pathLine);
 		token = _strtok(copy_pathLine + 5, ":");
 		while (token != NULL)
@@ -119,6 +143,14 @@ int get_PATH(char **env, list_t **paths_head)
 	/*print_list(*paths_head);*/
 	return (0);
 }
+
+/**
+ * is_found_and_excecutable - excecution i gess
+ * @av: args
+ * @paths_head: idk
+ * Return: idk
+ */
+
 int is_found_and_excecutable(char **av, list_t *paths_head)
 {
 	size_t n1, n2;
@@ -134,8 +166,8 @@ int is_found_and_excecutable(char **av, list_t *paths_head)
 		}
 	}
 	/*check all directories in path*/
-	while(trav_path != NULL)
-	{	
+	while (trav_path != NULL)
+	{
 		n1 = trav_path->len;
 		n2 = _strlen(av[0]);
 		testFile = (char *)_malloc(n1 + n2 + 2);
@@ -150,7 +182,6 @@ int is_found_and_excecutable(char **av, list_t *paths_head)
 			return (0);
 		}
 		trav_path = trav_path->next;
-	}	
+	}
 	return (-1);
 }
-
