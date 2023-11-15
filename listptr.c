75ec8b83fcb9e1094ab022c3340361b20193abff
Sasuke-kh <omerkhalid56682@gmail.com>
2023-11-16 00:50:03 +0200

    betty

--- listptr.c
+++ listptr.c
@@ -4,7 +4,7 @@
 #include <string.h>
 /**
 * list_lenptr - count of number of elements of a list
-* @h: pointer to list node
+* @p: pointer to list node
 *
 * Return: Number of node of list
 */
@@ -21,7 +21,7 @@ size_t list_lenptr(const listptr_t *p)
 }
 /**
 * print_listptr - prints all elements of a list
-* @h: pointer to list node
+* @p: pointer to list node
 *
 * Return: Number of node of list
 */
@@ -37,6 +37,13 @@ size_t print_listptr(const listptr_t *p)
 	}
 	return (n);
 }
+
+/**
+ * add_node_ptr - free used malloc
+ * @headptr: pointer to linked list
+ * @p: idk
+ * Return: idk
+ */
 
 listptr_t *add_node_ptr(listptr_t **headptr, void *p)
 {
@@ -57,8 +64,8 @@ listptr_t *add_node_ptr(listptr_t **headptr, void *p)
 }
 /**
  * add_node_end_ptr - adds a new node at end of a list
- * @head: double pointer to list
- * @str: string of the new head node
+ * @headptr: double pointer to list
+ * @p: string of the new head node
  *
  * Return: pointer to the new node, NULL if failed
  */
@@ -92,7 +99,7 @@ listptr_t *add_node_end_ptr(listptr_t **headptr, void *p)
 
 /**
 * free_list_ptr - frees memory ocuupied by single linked list
-* @head: pointer to head node of linked list
+* @headptr: pointer to head node of linked list
 */
 void free_list_ptr(listptr_t *headptr)
 {
